23506c698f19a7462f129242de3bf6761ce1cf56
Michael Jay <mjay@bitpay.com>
2025-03-08 16:22:33 -0500

    add main export of @oslana/web3.js package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   BalanceProgram: require('./bin/balance'),
   SendProgram: require('./bin/send'),
-  CryptoRpc: require('./lib')
+  CryptoRpc: require('./lib'),
+  SolWeb3: require('@solana/web3.js')
 };
